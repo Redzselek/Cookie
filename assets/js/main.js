153f27f8a556e71cc3e15feab9a816b788392d66
Egyedi Róbert <egyedirobika@gmail.com>
2023-09-07 11:31:04 +0200

    1óra

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function Ment() 
+{
+    setCookie("nev", document.getElementById("nev").value, 1);
+    document.getElementById("mianaved").style.display = "none"
+
+    document.getElementById("ezaneve").innerHTML = document.getElementById("nev").value
+
+    document.getElementById("vanneve").style.display = "block"
+}
+
+function Ellenoriz()
+{
+    var n = getCookie("nev")
+    if (n == "") 
+    {
+        document.getElementById("mianave").style.display = "block"    
+        document.getElementById("vanneve").style.display = "none"    
+    }
+    else
+    {
+        document.getElementById("mianave").style.display = "block"    
+        document.getElementById("ezaneve").style.display = "block"    
+        document.getElementById("vanneve").style.display = "none"    
+    }
+}
